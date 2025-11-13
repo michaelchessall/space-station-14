0e468a86180a445230dff0b292c52f378e9c843e
Michael Chessall <chessall007@gmail.com>
2025-11-13 14:05:56 -0700

    Revert "fx job title saving"
    This reverts commit bfc1fb88b3a79954ff96ecc23eb9257a7a9569a7.

--- Content.Shared/Access/Components/IdCardComponent.cs
+++ Content.Shared/Access/Components/IdCardComponent.cs
@@ -31,7 +31,6 @@ public sealed partial class IdCardComponent : Component
     [AutoNetworkedField]
     private string? _jobTitle;
 
-    [DataField]
     [Access(typeof(SharedIdCardSystem), typeof(SharedPdaSystem), typeof(SharedAgentIdCardSystem), Other = AccessPermissions.ReadWriteExecute)]
     public string? LocalizedJobTitle { set => _jobTitle = value; get => _jobTitle ?? Loc.GetString(JobTitle ?? string.Empty); }
 
